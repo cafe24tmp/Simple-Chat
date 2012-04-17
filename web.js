f588344a3fc6a8f050bfbafdea2c2bc89030700c
wiseo <wiseo@simplexi.com>
2012-04-17 12:46:54 +0900

    remove express

--- web.js
+++ web.js
@@ -1,16 +1,23 @@
-var app = require("express").createServer(),
-    io = require("socket.io").listen(app);
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs');
 
 var port = process.env.PORT;
 if (!port) {
     port = 3000;
 }
 
-app.listen(port);
-
-app.get("/", function(req, res){
-    res.sendfile(__dirname + "/index.html");
-});
+function handler(req,res) {
+	fs.readFile(__dirname + '/index.html',
+		function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
+}
 
 io.sockets.on("connection", function(socket){
     socket.on("chat", function(data){
@@ -18,3 +25,5 @@ io.sockets.on("connection", function(socket){
         socket.broadcast.emit("chat", data);
     });
 });
+
+app.listen(port);
